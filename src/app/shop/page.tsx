0e4311c69db377361ad5212a7b395fb90aebb14e
chongkiu33 <chongkiu33@users.noreply.github.com>
2024-07-30 14:39:16 +0800

    newshop

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,8 +1,13 @@
+import styles from './shop.module.css';
+
 export default function Shopping() {
     return (
-      <div>
-        <h1>Shop</h1>
-        <p>Welcome to the shopping page!</p>
+      <div className={styles.bigcontainer}>
+        <div className={styles.textContainer}>Please Select the region</div>
+      <div className={styles.container}>
+        <div className={styles.box}>China</div>
+        <div className={styles.box}>Europe</div>
+      </div>
       </div>
     );
   }
