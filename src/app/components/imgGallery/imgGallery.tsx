91372b5395c7ee1a5b579bc8c7c528b96ec2e43e
chongkiu33 <chongkiu33@users.noreply.github.com>
2024-08-12 01:01:43 +0800

    object

--- src/app/components/imgGallery/imgGallery.tsx
+++ src/app/components/imgGallery/imgGallery.tsx
@@ -48,13 +48,9 @@ const ImgGallery = () => {
 
 export default ImgGallery;
 
-interface ImgItemProps {
-    url: string;
-    position: [number, number, number];
-    scale: [number, number];
-}
 
-function ImgItem({ url, position, scale }: ImgItemProps){
+
+function ImgItem({ url, position, scale }: { url: string; position: any; scale: any }){
     const mesh = useRef<THREE.Mesh>(null);
    
     const rotationAngle = useRef(0); // 用于跟踪旋转角度
